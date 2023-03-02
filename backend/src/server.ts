69873afee6aa609aafa8f716198ec908f9c9f4cc
jfernandesdev <jfernandes.dev@gmail.com>
2023-03-02 13:29:14 -0300

    CHORE: Eslint and Prettier configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import fastify from "fastify";
 
 const app = fastify();
 
-app.get('/', (request, reply) => {
+app.get("/", (request, reply) => {
   return reply.send("Hello World!");
-})
+});
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log("Server is running in port 3333... 🚀")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running in port 3333... 🚀");
+  });
