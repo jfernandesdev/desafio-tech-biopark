6e2da6c4bbf64224014a63fab9252ce5604df60a
jfernandesdev <jfernandes.dev@gmail.com>
2023-03-04 10:51:13 -0300

    CHORE: initial frontend setup with Vite, Typescript and Styled Components

---
+++ frontend/src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  background: "#E5E5E5",
+
+  "brand-primary": "#243746",
+  "brand-secondary": "#EB0045",
+
+  "base-title": "#243746",
+  "base-text": "#4B5563",
+  "base-card-line": "#DDDDDD",
+  "base-card-background": "#FEFEFE",
+
+  "green-success": "#059669",
+  "green-background": "rgba(5, 150, 105, 0.1);",
+
+  "red-alert": "#DC2626",
+  "red-background": "rgba(220, 38, 38, 0.1);",
+
+  "yellow-warning": "#D97706",
+  "yellow-background": "rgba(217, 119, 6, 0.1)",
+
+  "blue-info": "#1D4ED8",
+  "blue-background": "rgba(29, 78, 216, 0.1)",
+} as const;
